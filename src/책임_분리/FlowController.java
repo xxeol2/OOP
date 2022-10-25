6599c51c3b4a7c585eb26c0fc33d31d35223ef50
xxeol2 <jhj01177@naver.com>
2022-10-25 18:35:45 +0900

    기존의 흐름제어 객체: 데이터 읽기 객체 생성 책임 + 흐름제어 책임 => 두가지 책임

--- src/책임_분리/FlowController.java
+++ src/책임_분리/FlowController.java
@@ -4,13 +4,18 @@ package 책임_분리;
 public class FlowController {
 
     public void process() {
+        // 데이터 읽기 객체 직접 생성
         FileDataReader reader = new FileDataReader();
+
+        // 흐름제어: 1 읽기
         byte[] data = reader.read();
 
         Encryptor encryptor = new Encryptor();
+        // 흐름제어: 2 암호화
         byte[] encryptedData = encryptor.encrypt(data);
 
         FileDataWriter writer = new FileDataWriter();
+        // 흐름제어: 3 쓰기
         writer.write(encryptedData);
     }
 }
