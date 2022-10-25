53ed2fb1c39b2c189ab037b3ec74dca943d4a740
xxeol2 <jhj01177@naver.com>
2022-10-25 18:40:27 +0900

    FlowController에서 isFile 삭제

--- src/추상타입_활용/FlowController.java
+++ src/추상타입_활용/FlowController.java
@@ -3,12 +3,6 @@ package 추상타입_활용;
 // 흐름 제어 객체
 public class FlowController {
 
-    private boolean useFile;
-
-    public FlowController(boolean useFile) {
-        this.useFile = useFile;
-    }
-
     public void process() {
         ByteSource source = ByteSourceFactory.getInstance().create();
         byte[] data = source.read();
