b9d503e84835479a3e7843c89c8d350a12d9c205
xxeol2 <jhj01177@naver.com>
2022-10-27 23:53:34 +0900

    추상타입으로 변경

--- src/서비스_로케이터/Problem1.java
+++ src/서비스_로케이터/Problem1.java
@@ -4,16 +4,19 @@ import java.util.List;
 
 import 서비스_로케이터.problem1.GameController;
 import 서비스_로케이터.problem1.Locator;
+import 서비스_로케이터.problem1.PageValidChecker;
 import 서비스_로케이터.problem1.PageValidCheckerImp;
+import 서비스_로케이터.problem1.ScoreCalculator;
 import 서비스_로케이터.problem1.ScoreCalculatorImp;
+import 서비스_로케이터.problem1.WinnerSelector;
 import 서비스_로케이터.problem1.WinnerSelectorImp;
 
 class Problem1 {
 	public static int solution(List<Integer> pobi, List<Integer> crong) {
 		int lastPage = 400;
-		PageValidCheckerImp pageValidChecker = new PageValidCheckerImp(lastPage);
-		ScoreCalculatorImp scoreCalculator = new ScoreCalculatorImp();
-		WinnerSelectorImp winnerSelector = new WinnerSelectorImp();
+		PageValidChecker pageValidChecker = new PageValidCheckerImp(lastPage);
+		ScoreCalculator scoreCalculator = new ScoreCalculatorImp();
+		WinnerSelector winnerSelector = new WinnerSelectorImp();
 
 		Locator locator = new Locator(pageValidChecker, scoreCalculator, winnerSelector);
 		Locator.init(locator);
