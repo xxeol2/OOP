197dbc39903ac598eb713ed92c514b18d3462c92
xxeol2 <jhj01177@naver.com>
2022-11-01 16:05:10 +0900

    [전략패턴] 콘텍스트 코드의 변경 없이 새로운 전략 추가 가능

--- src/디자인패턴/전략패턴/Client.java
+++ src/디자인패턴/전략패턴/Client.java
@@ -4,10 +4,17 @@ public class Client {
 	private DiscountStrategy strategy;
 
 	public void onFirstGuestButtonClick() {
+		// 첫 손님 할인 버튼 누를 때 생성
 		strategy = new FirstGuestDiscountStrategy();
 	}
 
+	public void onLastGuestButtonClick() {
+		// 마지막 손님 대폭 할인 버튼 누를 때 생성
+		strategy = new LastGuestDiscountStrategy();
+	}
+
 	public void onCalculationButtonClick() {
+		// 계산 버튼 누를 때 실행
 		Calculator cal = new Calculator(strategy);
 		int price = cal.calculate(items);
 	}
