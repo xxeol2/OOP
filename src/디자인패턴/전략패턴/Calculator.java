bcb0f7e65e96546ac07a32386335e384651d3eb8
xxeol2 <jhj01177@naver.com>
2022-11-01 15:50:43 +0900

    [전략패턴] 기존 코드

---
+++ src/디자인패턴/전략패턴/Calculator.java
@@ -0,0 +1,19 @@
+package 디자인패턴.전략패턴;
+
+import java.util.List;
+
+public class Calculator {
+
+	public int calculate(boolean firstGuest, List<Item> items) {
+		int sum = 0;
+		for (Item item: items) {
+			if (firstGuest)
+				sum += (int)(item.getPrice() * 0.9);
+			else if (!item.isFresh())
+				sum += (int)(item.getPrice() * 0.8);
+			else
+				sum += item.getPrice();
+		}
+		return sum;
+	}
+}
---
+++ src/디자인패턴/전략패턴/Item.java
@@ -0,0 +1,14 @@
+package 디자인패턴.전략패턴;
+
+public class Item {
+	private int price = 10000;
+	private boolean isFresh = true;
+
+	public int getPrice() {
+		return price;
+	}
+
+	public boolean isFresh() {
+		return true;
+	}
+}
